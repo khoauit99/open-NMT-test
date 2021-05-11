fc3650d252bf19ce1c21a4cde5ac4cac43af4a33
phạm khoa @kpuit99 <kpuit99@gmail.com>
2021-05-11 13:22:38 +0700

    update

--- onmt/utils/logging.py
+++ onmt/utils/logging.py
@@ -4,15 +4,10 @@ from logging.handlers import RotatingFileHandler
 logger = logging.getLogger()
 
 
-def init_logger(
-    log_file=None,
-    log_file_level=logging.NOTSET,
-    rotate=False,
-    log_level=logging.INFO,
-):
+def init_logger(log_file=None, log_file_level=logging.NOTSET, rotate=False):
     log_format = logging.Formatter("[%(asctime)s %(levelname)s] %(message)s")
     logger = logging.getLogger()
-    logger.setLevel(log_level)
+    logger.setLevel(logging.INFO)
 
     console_handler = logging.StreamHandler()
     console_handler.setFormatter(log_format)
